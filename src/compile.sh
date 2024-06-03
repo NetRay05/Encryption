184ac7311fc2a48b14401715ec73b44ff6ad680c
Keshire <168670422+NetRay05@users.noreply.github.com>
2024-06-04 00:40:36 +0200

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COMMAND=`g++ enc.cpp -o enc -Os -Wall -Wno-deprecated -std=c++20 -lz -lzip -lcryptopp -fsanitize=address && g++ dec.cpp -o dec -Os -Wall -Wno-deprecated -lz -lzip -lcryptopp -std=c++20 -fsanitize=address`
+COMMAND=`g++ enc.cpp -o enc -Os -Wall -Wno-deprecated -std=c++20 -lz -lzip -lcryptopp && g++ dec.cpp -o dec -Os -Wall -Wno-deprecated -lz -lzip -lcryptopp -std=c++20`
 echo "Compiling..."
 echo $COMMAND
 echo "Compiled!"
